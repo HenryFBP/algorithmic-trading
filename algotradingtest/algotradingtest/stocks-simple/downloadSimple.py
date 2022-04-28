de19fa1ef425f0e2b21c6962c21ace05060f1da8
Henry Post <HenryFBP@gmail.com>
2022-04-28 14:08:59 -0400

    dl xlsx stock

--- algotradingtest/algotradingtest/stocks-simple/downloadSimple.py
+++ algotradingtest/algotradingtest/stocks-simple/downloadSimple.py
@@ -10,6 +10,8 @@ import os
 import warnings
 
 # Ignore printing all warnings
+from nbformat import write
+
 warnings.filterwarnings('ignore')
 
 # print all outputs
@@ -22,3 +24,14 @@ _start = dt.date(2015, 1, 2)
 _end = dt.date(2020, 4, 30)
 ticker = 'MSFT'
 df = yf.download(ticker, start=_start, end=_end)
+
+fp = f'./{ticker}-sample.xlsx'
+if os.path.exists(fp):
+    print(f"{fp} exists.")
+    exit(1)
+
+writer = pd.ExcelWriter(fp)
+df.to_excel(writer)
+
+writer.save()
+print("Done, open " + fp)
