9d780fa17348c998d763a8717345ec2fff9c3dc3
Henry Post <HenryFBP@gmail.com>
2022-04-28 13:59:29 -0400

    simplify lock files

--- algotradingtest/algotradingtest/stocks-simple/downloadDatShit.py
+++ algotradingtest/algotradingtest/stocks-simple/downloadDatShit.py
@@ -14,10 +14,11 @@ warnings.filterwarnings('ignore')
 
 # print all outputs
 from IPython.core.interactiveshell import InteractiveShell
+
 InteractiveShell.ast_node_interactivity = "all"
 
 # downloading historical necessary data for backtesting and analysis
-_start = dt.date(2015,1,2)
-_end = dt.date(2020,4,30)
+_start = dt.date(2015, 1, 2)
+_end = dt.date(2020, 4, 30)
 ticker = 'MSFT'
-df = yf.download(ticker, start = _start, end = _end) 
+df = yf.download(ticker, start=_start, end=_end)
