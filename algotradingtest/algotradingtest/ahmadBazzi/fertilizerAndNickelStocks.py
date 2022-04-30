f03f856cd9a2c325340617bb4540d2803944e4b4
Henry Post <HenryFBP@gmail.com>
2022-04-30 14:18:44 -0400

    get graph to work

--- algotradingtest/algotradingtest/ahmadBazzi/fertilizerAndNickelStocks.py
+++ algotradingtest/algotradingtest/ahmadBazzi/fertilizerAndNickelStocks.py
@@ -10,12 +10,16 @@ from stockLib import *
 start = datetime.datetime(2012, 1, 1)
 end = datetime.datetime(2022, 1, 1)
 
-stock_list = 'SBSW MTRN NIC GMKN CNC POS NICK VALE 2362 GLEN BHP RRR TSLA' \
+car_stock_list = 'TSLA GM F'.split(' ')
+nickel_stock_list = 'SBSW MTRN NIC GMKN CNC POS NICK VALE 2362 GLEN BHP RRR' \
     .split(' ')  # YOINK from https://www.finder.com/nickel-mining-stocks
-print(stock_list)
+print(nickel_stock_list)
+
+all_stock_list = car_stock_list + \
+                 nickel_stock_list
 
 stock_dict = dict()
-for stock_ticker in stock_list:
+for stock_ticker in all_stock_list:
     try:
         stock_dict[stock_ticker] = gimme_da_stock_disk_cache(stock_ticker, 'yahoo', start, end)
         print("Got " + stock_ticker + "!")
@@ -24,7 +28,11 @@ for stock_ticker in stock_list:
         stock_dict[stock_ticker] = rde
 
 tesla = stock_dict['TSLA']
+gm = stock_dict['GM']
+ford = stock_dict['F']
 
 print(tesla.head())
+print(gm.head())
+print(ford.head())
 
-generate_stock_dfs_graphs([tesla])
+generate_stock_dfs_graphs([tesla, gm, ford])
--- algotradingtest/algotradingtest/ahmadBazzi/stockLib.py
+++ algotradingtest/algotradingtest/ahmadBazzi/stockLib.py
@@ -1,3 +1,4 @@
+import shutil
 from functools import lru_cache
 import pandas_datareader.data as web
 from pandas_datareader._utils import RemoteDataError
@@ -7,7 +8,6 @@ import numpy as np
 import pandas as pd
 from typing import List, Dict
 import os
-import logging
 
 '''
 useful stock utils
@@ -29,35 +29,52 @@ def gimme_da_stock_lru_cache(ticker: str,
 # and this is persistent across disks...
 def gimme_da_stock_disk_cache(ticker, source, start, end, cache_folder="./.stock-cache/") -> pd.DataFrame:
     fp = f"{ticker}-{source}-{start}-{end}.cache.csv"
+
+    if not os.path.exists(cache_folder):
+        os.makedirs(cache_folder)
+
     fp = cache_folder + fp
     fp = os.path.abspath(fp)
 
-    os.makedirs(fp)
-
     if os.path.exists(fp):
-        logging.info("HIT for {} at {}".format(ticker, fp))
+        print("HIT for {} at {}".format(ticker, fp))
         return pd.read_csv(fp)
     else:
-        logging.info("MISS for " + ticker)
+        print("MISS for " + ticker)
         df = gimme_da_stock_lru_cache(ticker, source, start, end)
         df.to_csv(fp, )
         return pd.read_csv(fp)
 
 
+# khanacademy stats courses -- do it!!!!!
+# need calc 1 at least
+# if you do any distributions, you must do calc
+# R and Stata libs are good
+
+
 def generate_stock_dfs_graphs(stock_dfs: List[pd.DataFrame], figsize=rectFig, folder=None) -> str:
+    stock_names: List[str] = [sdf['Stock Ticker'][0] for sdf in stock_dfs]
+
     if not folder:
-        folder = '-'.join(sdf['Stock Ticker'][0] for sdf in stock_dfs)
+        folder = '-'.join(stock_names)
         folder = "./" + folder
         folder = os.path.abspath(folder)
 
     print(f"Saving to {folder}.")
+
+    if os.path.exists(folder):
+        shutil.rmtree(folder)
+
     if not os.path.exists(folder):
         os.mkdir(folder)
 
+    # make a simple price graph
     for stock_df in stock_dfs:
-        #
-        pass
-
-
+        stock_df['Open'].plot(label=stock_df['Stock Ticker'][0], figsize=rectFig)
+    plt.legend()
+    plt.ylabel("Stock Price in USD")
+    plt.xlabel("Date")
+    plt.title("Stock prices of {}".format(','.join(stock_names)))
+    plt.savefig(os.path.join(folder, 'price.generated.png'))
 
     return folder
