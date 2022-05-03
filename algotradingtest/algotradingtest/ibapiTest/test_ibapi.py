905576f44eb3d3ae282b1ecb622216063e68a9b3
Henry Post <HenryFBP@gmail.com>
2022-05-03 09:43:19 -0400

    fix folder

---
+++ algotradingtest/algotradingtest/ibapiTest/test_ibapi.py
@@ -0,0 +1,20 @@
+# https://pypi.org/project/ibapi/
+
+import ibapi
+
+# https://algotrading101.com/learn/interactive-brokers-python-api-native-guide/
+
+from ibapi.client import EClient
+from ibapi.wrapper import EWrapper
+
+class IBapi(EWrapper, EClient):
+     def __init__(self):
+         EClient.__init__(self, self)
+
+app = IBapi()
+app.connect('127.0.0.1', 7497, 123)
+app.run()
+
+import time
+time.sleep(2)
+app.disconnect()
